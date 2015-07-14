764c7261be8b2ee9e04063c5802cd665f1ebe7c7
oneralon <oneralon@gmail.com>
2015-07-14 15:10:33 +0300

    Fix start script

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# coffee list-collector/index.coffee
-# coffee list-parser/index.coffee
+coffee list-collector/index.coffee
+coffee list-parser/index.coffee
 coffee auction-collector/index.coffee
 coffee auction-parser/index.coffee
 coffee lot-collector/index.coffee
