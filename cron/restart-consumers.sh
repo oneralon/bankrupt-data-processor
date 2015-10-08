ed9c2a2ebdc7dad87cf8814a286820b938bb7720
oneralon <oneralon@gmail.com>
2015-10-08 15:09:00 +0300

    Fix restarter

--- cron/restart-consumers.sh
+++ cron/restart-consumers.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 cd "$BDP_BASE"
 echo "$(date) Restart consumers" >> logs/cron.log
-sudo /etc/init.d/rabbitmq-server restart
-sleep 2
 pkill -9 -f 'node /usr/local/bin/coffee consumers/lists-html.coffee'
 pkill -9 -f 'node /usr/local/bin/coffee consumers/trades-html.coffee'
 pkill -9 -f 'node /usr/local/bin/coffee consumers/trades-json.coffee'
@@ -10,5 +8,4 @@ pkill -9 -f 'node /usr/local/bin/coffee consumers/trades-url.coffee'
 pkill -9 -f 'node /usr/local/bin/coffee consumers/lots-url.coffee'
 pkill -9 -f 'node /usr/local/bin/coffee consumers/lots-html.coffee'
 pkill -9 -f 'node /usr/local/bin/coffee consumers/lots-json.coffee'
-pkill phantomjs
 grunt consumers:start
